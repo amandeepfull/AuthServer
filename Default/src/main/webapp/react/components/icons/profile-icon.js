2698749138ab6860a1624af01b3509724c686d3a
Amandeep <amandeep.santokh@full.co>
2019-09-16 17:47:29 +0530

    added profile logout and username options

--- Default/src/main/webapp/react/components/icons/profile-icon.js
+++ Default/src/main/webapp/react/components/icons/profile-icon.js
@@ -4,14 +4,14 @@ import React from 'react'
 export default class ProfileIcon extends React.Component {
     constructor(props) {
         super(props);
-        this.handleClick = this.handleClick.bind(this);
+        this.showProfileMenu = this.showProfileMenu.bind(this);
 
     }
 
     render() {
         return (
             <div className="profile-menu">
-                 <a onClick={this.handleClick} id={this.props.id} className="profile-icon" href="#" ><img id={this.props.id} src={this.props.iconImgSrc} width="60px" /></a>
+                <a onClick={this.showProfileMenu} id={this.props.id} className="profile-icon" href="#" ><img id={this.props.id} src={this.props.iconImgSrc} width="60px" /></a>
                 <div className="profile-menu-wrapper">
                     <ul className="profile-menu-list">
                         <li className="profile-menu-li">
@@ -20,10 +20,10 @@ export default class ProfileIcon extends React.Component {
                         <li className="profile-menu-li">
                             <a href="#">Logout</a>
                         </li>
-                    
+
                     </ul>
                 </div>
-               
+
             </div>
 
 
@@ -31,12 +31,16 @@ export default class ProfileIcon extends React.Component {
         )
     }
 
-    handleClick(event) {
+    showProfileMenu(event) {
 
         if (event.target.id == "" || event.target.id == "undefined")
             return;
 
-
+        const applistWrapperDisplayStyle = document.getElementsByClassName("profile-menu-wrapper")[0].style.display;
+        if (applistWrapperDisplayStyle === "")
+            document.getElementsByClassName("profile-menu-wrapper")[0].style.display = "block"
+        else
+            document.getElementsByClassName("profile-menu-wrapper")[0].style.display = "";
     }
 
 }
